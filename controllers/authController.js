81f785e0fed1ac8268783016f4319c10e7793654
codeboyz12 <chanutsunatho@gmail.com>
2025-11-14 14:30:29 +0700

    login feature and check for users not found, wrong password and login success

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,3 +26,20 @@ exports.getAuthByUsername = async (req, res) => {
     console.log(row);
     res.json(row);
 }
+
+exports.login = async (req, res) => {
+    const {username, password} = await req.body;
+    const row = await authModel.selectByUsername(username);
+    if (!row) {
+        console.log("Users not found");
+        res.send({"status": "Users not found"});
+    }
+
+    if (password == row.password) {
+        console.log("Login success");
+        res.send({"status": "login success"});
+    } else {
+        console.log("Login unsuccess");
+        res.send({"status": "wrong password"});
+    }
+}
